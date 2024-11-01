8f44663dfef6a4992e9b87e9795c51a101ef51dc
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2024-11-01 10:21:37 +0530

    initial ommit

---
+++ src/main/java/com/DSA/example/DsaPracticeSheetApplication.java
@@ -0,0 +1,13 @@
+package com.DSA.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaPracticeSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaPracticeSheetApplication.class, args);
+	}
+
+}
